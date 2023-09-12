8034811f782ab43757810efba3f26016b459c106
Victor 'Macrev <v0042447@ems.com.br>
2023-09-12 15:04:08 -0300

    Create config.py
    Here will be made the database connection

---
+++ config.py
@@ -0,0 +1,11 @@
+dbconfig = {
+    'drivername':'mysql',
+    'host':'localhost',
+    'port':3306,
+    'database':'app_pmb',
+    'username':'macrev',
+    'password':'PAx3UyvM53IJwN80'
+    
+}
+
+
